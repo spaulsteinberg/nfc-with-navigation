f221715a7d66619a44c6fed80f9d9f217f52f66f
spaulsteinberg <sampaulsteinberg@gmail.com>
2022-10-26 09:38:53 -0500

    add formik

--- screens/SendDataScreen.tsx
+++ screens/SendDataScreen.tsx
@@ -1,62 +1,41 @@
+import { useFormik } from 'formik'
 import React, { useState } from 'react'
-import { ScrollView, StyleSheet, View, ActivityIndicator } from 'react-native'
-import { NFInlineTextInput } from '../components/ui'
-import NFButton from '../components/ui/NFButton'
-import Colors from '../constants/Colors'
+import { ScrollView } from 'react-native'
 import { RootStackScreenProps } from '../types'
+import * as Yup from 'yup'
+import TableForm from '../components/send/TableForm'
 
 const SendDataScreen = ({ route, navigation }: RootStackScreenProps<'SendData'>) => {
-  
+
   if (!route.params || !route.params.data) {
     navigation.navigate("Landing")
   }
 
-  const [tableNumber, setTableNumber] = useState(route.params.data)
-  const [tableBuserName, setTableBuserName] = useState('')
-  const [tableStatus, setTableStatus] = useState('')
-
   const [requestLoading, setRequestLoading] = useState(false)
 
-  const handleConfirmPress = () => {
-    console.log("press")
-    setRequestLoading(true)
-    setTimeout(() => setRequestLoading(false), 4000)
-  }
+  const formik = useFormik({
+    initialValues: {
+      number: route.params.data,
+      buser: '',
+      status: ''
+    },
+    validationSchema: Yup.object().shape({
+      number: Yup.string().trim().required("Table number is required"),
+      buser: Yup.string().trim().required("Buser name is required.").max(50, "This field cannot be more than 50 characters."),
+      status: Yup.string().trim().required("Status is a required field.")
+    }),
+    onSubmit: values => {
+      console.log(values)
+      setRequestLoading(true)
+      setTimeout(() => setRequestLoading(false), 4000)
+    }
+  })
 
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }} style={{ backgroundColor: "#fff" }}>
-      <View style={styles.container}>
-        <NFInlineTextInput label='Table' vStyle={styles.secondaryTextView} iStyle={styles.input}value={tableNumber} onChangeText={setTableNumber} editable={false} />
-        <NFInlineTextInput label='Buser' vStyle={styles.secondaryTextView} iStyle={styles.input} value={tableBuserName} onChangeText={setTableBuserName} />
-        <NFInlineTextInput label='Status' vStyle={styles.secondaryTextView} iStyle={styles.input} value={tableStatus} onChangeText={setTableStatus} />
-        <View>
-          <NFButton outerStyle={styles.buttonOuterView} title="Confirm" onPress={handleConfirmPress}>
-            { requestLoading ? <ActivityIndicator size="small" color={Colors.main.text} /> : null }
-          </NFButton>
-        </View>
-      </View>
+      <TableForm formik={formik} loading={requestLoading} />
     </ScrollView>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 12,
-    marginVertical: 48
-  },
-  input: {
-    padding: 8,
-    borderRadius: 8,
-    borderWidth: 1,
-    borderColor: "gray"
-  },
-  secondaryTextView: {
-    marginVertical: 12
-  },
-  buttonOuterView: {
-    marginVertical: 24
-  }
-})
 
 export default SendDataScreen
