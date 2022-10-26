d26e548f16644c01ca26d9ff8c5efadfd6a416a1
spaulsteinberg <sampaulsteinberg@gmail.com>
2022-10-26 14:20:00 -0500

    add validation and custom form

--- screens/SendDataScreen.tsx
+++ screens/SendDataScreen.tsx
@@ -1,8 +1,6 @@
-import { useFormik } from 'formik'
 import React, { useState } from 'react'
 import { ScrollView } from 'react-native'
 import { RootStackScreenProps } from '../types'
-import * as Yup from 'yup'
 import TableForm from '../components/send/TableForm'
 
 const SendDataScreen = ({ route, navigation }: RootStackScreenProps<'SendData'>) => {
@@ -13,29 +11,82 @@ const SendDataScreen = ({ route, navigation }: RootStackScreenProps<'SendData'>)
 
   const [requestLoading, setRequestLoading] = useState(false)
 
-  const formik = useFormik({
-    initialValues: {
+  const [form, setForm] = useState({
+    values: {
       buser: '',
       status: ''
     },
-    validationSchema: Yup.object().shape({
-      buser: Yup.string().trim().required("Buser name is required.").max(50, "This field cannot be more than 50 characters."),
-      status: Yup.string().trim().required("Status is a required field.")
-    }),
-    onSubmit: values => {
-      console.log(values)
-      setRequestLoading(true)
-      setTimeout(() => setRequestLoading(false), 4000)
+    errors: {
+      buser: '',
+      status: ''
+    },
+    touched: {
+      buser: false,
+      status: false
     }
   })
 
-  const setStatusDropdown = (i:string):void => {
-    formik.setFieldValue("status", i)
+  const handleChangeText = (key:string, newValue:string) => {
+    let error = ''
+    if (!newValue) {
+      error = `${key} is a required field.`
+    }
+    else if (key === 'buser' && newValue.length > 30) {
+      error = `${key} cannot be more than 30 characters long.`
+    }
+    setForm({
+      ...form,
+      values: {
+        ...form.values,
+        [key]: newValue.trim()
+      },
+      touched: {
+        ...form.touched,
+        [key]: true
+      },
+      errors: {
+        ...form.errors,
+        [key]: error
+      }
+    })
+  }
+
+  const validateForm = ():boolean => {
+    const nonBlankErrors = Object.keys(form.errors).filter((e:string) => form.errors[e as keyof typeof form.errors] !== '')
+    if (nonBlankErrors.length > 0) {
+      console.log("errors exist", nonBlankErrors)
+      return false
+    }
+
+    const unTouchedFields = Object.keys(form.touched).filter((t:string) => !form.touched[t as keyof typeof form.touched])
+    if (unTouchedFields.length > 0) {
+      setForm({
+        ...form,
+        errors: {
+          buser: form.values.buser === "" ? `buser is a required field.` : "",
+          status: form.values.status === "" ? `status is a required field.` : ""
+        },
+        touched: {
+          buser: true,
+          status: true
+        }
+      })
+      return false
+    }
+
+    return true
+  }
+
+  const handleSubmitPress = () => {
+    console.log(form)
+    if (!validateForm()) return
+    setRequestLoading(true)
+    setTimeout(() => setRequestLoading(false), 2000)
   }
 
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }} style={{ backgroundColor: "#fff" }}>
-      <TableForm formik={formik} loading={requestLoading} number={route.params.data} onDropSelect={setStatusDropdown} />
+      <TableForm form={form} loading={requestLoading} number={route.params.data} handleChangeText={handleChangeText} handleSubmitPress={handleSubmitPress} />
     </ScrollView>
   )
 }
