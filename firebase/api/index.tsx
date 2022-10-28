bb316899d97ca323b45578412c5666923a5fbbd2
spaulsteinberg <sampaulsteinberg@gmail.com>
2022-10-27 21:52:34 -0500

    get all logs

--- firebase/api/index.tsx
+++ firebase/api/index.tsx
@@ -1,7 +1,14 @@
 import { db } from "../config"
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, getDocs, DocumentData } from "firebase/firestore";
 import LogPayload from "../../models/LogPayload";
 
-const postLog = async (payload: LogPayload) => await addDoc(collection(db, "logs"), { ...payload })
+const LOG_COLLECTION = "logs"
 
-export { postLog }
+const postLog = async (payload: LogPayload) => await addDoc(collection(db, LOG_COLLECTION), { ...payload })
+
+const getLogs = async () => {
+    const { docs, empty } = await getDocs(collection(db, LOG_COLLECTION));
+    return empty ? [] : docs.map((doc:DocumentData) => doc.data()) as LogPayload[]
+}
+
+export { postLog, getLogs }
--- screens/LogScreen.tsx
+++ screens/LogScreen.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View, Text } from 'react-native'
+import { getLogs } from '../firebase/api'
 import { RootTabScreenProps } from '../types'
 
 const LogScreen = ({ navigation }:RootTabScreenProps<'Logging'>) => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const logs = await getLogs()
+      console.log(logs)
+    }
+
+    fetchData()
+  }, [])
+  
   return (
     <View>
         <Text>Hello tab 2</Text>
