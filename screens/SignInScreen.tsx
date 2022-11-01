bcff8d497274e7cd9b4d0af67591dedf42d28cb7
spaulsteinberg <sampaulsteinberg@gmail.com>
2022-10-31 20:42:31 -0500

    handle submit and email blur

--- screens/SignInScreen.tsx
+++ screens/SignInScreen.tsx
@@ -1,10 +1,7 @@
 import React, { useState } from 'react'
-import { View, StyleSheet, Dimensions, ScrollView, Button, ImageBackground } from 'react-native'
-import { Card, TextInput, Title } from 'react-native-paper'
+import { View, StyleSheet, Dimensions, ScrollView, ImageBackground } from 'react-native'
 import SignInCard from '../components/auth/SignInCard'
-import { NFButton } from '../components/ui'
-import Colors from '../constants/Colors'
-import InteractiveForm from '../models/InteractiveForm'
+import InteractiveForm, { validateInteractiveForm, ValidationResponse } from '../models/InteractiveForm'
 
 const WIDTH = Dimensions.get('window').width
 
@@ -23,32 +20,65 @@ const SignInScreen = () => {
             password: ''
         }
     })
-    const handleChangeText = (key: string, newValue: string):void => {
+
+    const handleChangeText = (key: string, newValue: string): void => {
         let error = ''
         if (!newValue) {
-          error = `${key} is a required field.`
+            error = `${key} is a required field.`
         }
         setForm({
-          ...form,
-          values: {
-            ...form.values,
-            [key]: newValue
-          },
-          touched: {
-            ...form.touched,
-            [key]: true
-          },
-          errors: {
-            ...form.errors,
-            [key]: error
-          }
+            ...form,
+            values: {
+                ...form.values,
+                [key]: newValue
+            },
+            touched: {
+                ...form.touched,
+                [key]: true
+            },
+            errors: {
+                ...form.errors,
+                [key]: error
+            }
         })
-      }
+    }
+
+    const handleSubmitPress = () => {
+        const v: ValidationResponse = validateInteractiveForm(form)
+        console.log(form)
+        if (v === ValidationResponse.Errors) return;
+        else if (v === ValidationResponse.Touched) {
+            setForm({
+                ...form,
+                errors: {
+                    email: form.values.email === "" ? "email is a required field." : "",
+                    password: form.values.password === "" ? "password is a required field." : ""
+                },
+                touched: {
+                    email: true,
+                    password: true
+                }
+            })
+        }
+
+        console.log(v)
+    }
+
+    const handleEmailBlur = () => {
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(form.values.email) !== true && setForm({
+            ...form,
+            errors: {
+                ...form.errors,
+                email: 'invalid email address'
+            }
+        })
+    }
+
     return (
         <ScrollView contentContainerStyle={styles.scroll}>
             <ImageBackground source={require('../assets/images/stacked-waves-haikei.png')} resizeMode="cover" style={styles.backgroundImage}>
                 <View style={styles.container}>
-                    <SignInCard form={form} handleChangeText={handleChangeText} />
+                    <SignInCard form={form} handleChangeText={handleChangeText} handleSubmitPress={handleSubmitPress} handleEmailBlur={handleEmailBlur} />
                 </View>
             </ImageBackground>
         </ScrollView>
